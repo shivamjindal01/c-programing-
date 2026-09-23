ae08a1a7246c8c34d75cc0693afd45d921f5ec1b
Shivam Jindal <shivjindal456@gmail.com>
2026-09-24 01:30:21 +0530

    Create decreasing_star_mirror.c

---
+++ decreasing_star_mirror.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i;
+int j;
+int k;
+int n;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    for(j=1;j<=i;j++)
+     printf(" ");
+
+    for(k=i;k<=n;k++)
+        printf("*");
+  printf("\n");
+}
+return 0;
+
+
+}
